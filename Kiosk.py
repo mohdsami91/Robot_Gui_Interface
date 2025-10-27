edc1ed11e78b51332d79a6a08d74eab2694c0c3c
mohdsami91 <msusami9911@gmail.com>
2025-10-27 19:30:07 +0530

    Kiosk mode full screen py

---
+++ Kiosk.py
@@ -0,0 +1,17 @@
+### Fullscreen & Kiosk Mode Setup ##
+
+from PyQt5 import QtWidgets, QtCore
+import sys
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)  # Removes minimize/maximize buttons
+        self.showFullScreen()  # Forces full screen
+        self.setCursor(QtCore.Qt.BlankCursor)  # (Optional) Hide cursor for clean UI
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
